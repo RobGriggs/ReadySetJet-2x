41adb925f655208bad9be51b3992e83fc23e8f0b
Rob Griggs <rob.j.griggs@gmail.com>
2015-04-01 00:05:13 -0500

    'config' now pulls credentials from another file

--- config.php
+++ config.php
@@ -1,16 +1,21 @@
 <?php
+/*
+ * Holds configuration variables used by application for database access and API query authentication
+ */
+date_default_timezone_set("America/Chicago"); 
 
-date_default_timezone_set(''); 
+//git ignored file with credential information
+require_once('credentials.php');
 
 $db_server_data = array( 
-                      'db_hostname' => '',
-                      'db_name' => '',
-                      'db_user' => '',
-                      'db_password' => '',
+                      'db_hostname' => $keys['dbHost'],
+                      'db_name' => $keys['dbTable'],
+                      'db_user' => $keys['dbUser'],
+                      'db_password' => $keys['dbPass'],
                     );
 
 $transactionID = microtime(true);
 
-$weatherAPIKey = '';
-$hotwireAPIKey = '';
-$microsoftAPIKey = ''; 
+$weatherAPIKey = $keys['weatherAPI'];
+$hotwireAPIKey = $keys['hotwireAPI'];
+$microsoftAPIKey = $keys['bingAPI'];
