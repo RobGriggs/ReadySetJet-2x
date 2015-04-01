bac04a03253ce77a8380edb61d377a4711f11e83
Rob Griggs <rob.j.griggs@gmail.com>
2015-03-31 20:46:43 -0500

    added doc blocks

--- php/core/Registry.php
+++ php/core/Registry.php
@@ -1,12 +1,26 @@
 <?php
-
+/**
+ * Holds Regiestry Class
+ */
+ 
+ /**
+  * A simple key => value regiestry for important objects
+  */
 class Registry
 {
+	
+	/**@var self singleton instance*/
     private static $instance = null;
+	
+	/**@var array Objects registered*/
     private $globalObjects = array();
     
+	/**Initialize, private to support singleton */
     private function __construct(){}
     
+	/**
+	 * Initilizes or returns singleton of Registry class 
+	 */
     public static function get_instance()
     {
         if( self::$instance === null ){
@@ -17,15 +31,25 @@ class Registry
         }
     }
     
+	/**
+	 * Sets an object in registry
+	 */
     public function set( $key, $value )
     {
         $this->globalObjects[$key] = $value;
     }
     
+	/**
+	 * gets an object from registry, static class uses instantiated class method get_local()
+	 * to allow a static get() method call
+	 */
     public static function get($key){
         return self::$instance->get_local($key);
     }
     
+	/**
+	 * Gets object from registry
+	 */
     private function get_local( $key )
     {
         if(isset( $this->globalObjects[$key] ) ){
