cb21d046e4590fe0382e90146f6dd487c8c9f5f8
Rob Griggs <rob.j.griggs@gmail.com>
2015-04-11 03:51:21 -0500

    Cleaned up weather API handling class, added documentation, set access modifiers to methods, removed all instance variables passed as method parameters

--- php/maintenance/maintenance_classes/WeatherAPI.php
+++ php/maintenance/maintenance_classes/WeatherAPI.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Holds Weather API Handling Class
+ * @author Rob Griggs
+ */
 
 /*
  * Weather data is provided by Weather Underground API
@@ -6,84 +10,89 @@
  * 
  * Call Limit: 500 calls a day
  * Call Speedlimit: 10 calls a minuet
- */
- 
+ */ 
 class WeatherAPI
 {
-    private $apiKey;
-    private $airports = array(); 
-	private $dataHandler;
-    private $queryDates;
-	private $apiCallLimitTime = 10; //seconds for rest
+	/**
+	 * @var string $apiKey Unique key used to authenticate with API server
+	 * @var array $airports Each array key holds a sub array that contains airport_id from database and respective airport_code 
+	 * @var DataHandler $dataHandler provides database query and update services
+	 * @var int $apiCallLimitTime Time, in seconds, to pass between successful API calls, in order to avoid exceeding API call speedlimit
+	 */
 	
-	//call limit time, to be set as long, or as longer than the allowed time between API calls 
-	private $weatherAPICallLimitTime = 12; //seconds
+    private $apiKey,
+    		$airports = array(), 
+			$dataHandler,
+    		$queryDates,
+			$apiCallLimitTime = 10;
 	
-    function __construct($apiKey)
+	/**
+	 * Initialize Class
+	 * @var string $apiKey authentication key for calling weather underground API 
+	 */
+    public function __construct($apiKey)
     {
         $this->apiKey = $apiKey;
         $this->dataHandler = Registry::get('DataHandler');
         $this->airports= $this->dataHandler->get_airports();
-        
-        //for debugging
-        //$this->airports = array( array( 'airport_id' =>'78', 'airport_code' => 'HNL'));
     }
 	
+	/**
+	 * Call to initiate update update of weather information in database
+	 */
 	public function update_forecasts()
 	{
 	    $time = microtime(true);
         
-        foreach ( $this->airports as $key => $values) {
+        foreach ($this->airports as $key => $values) {
             
             $airportCode = $values['airport_code'];
             
-            $rawWeatherData = $this->get_weather_forecast( $this->apiKey, $airportCode, $this->weatherAPICallLimitTime );
-            $preparedWeatherData = $this->parse_weather_forecast( $rawWeatherData );
-            $this->update_database( $this->dataHandler, $airportCode, $preparedWeatherData );
-            
+            $rawWeatherData = $this->get_weather_forecast($airportCode);
+            $preparedWeatherData = $this->parse_weather_forecast($rawWeatherData);
+            $this->update_database($airportCode, $preparedWeatherData );     
         }
         
         $timeNow = (microtime(true));
         $timePassed = $timeNow - $time;
-        echo "finished weather update in: ".$timePassed.' seconds';
-        
+        echo "finished weather update in: ".$timePassed.' seconds';   
 	}
 	
-	//get a list of airports from the DB
+	/**
+	 * Get an array of airport_codes from the DB, along with their respective database ids
+	 * @return array airport data
+	 */
 	private function get_airports()
 	{
         return $this->dataHandler->get_airports();
 	}
 
-    private function update_database( $dataHandler, $airportCode, $preparedWeatherData )
-    {
-        print_r($airportCode).PHP_EOL.PHP_EOL;
-        print_r($preparedWeatherData);
-        
-        $dataHandler->update_ten_day_forecast($airportCode, $preparedWeatherData);
-    }
-
-    //retrieve a weather forecast based on an airport location
-	private function get_weather_forecast($apiKey, $airport, $apiCallLimitTime) 
+	/**
+	 * Builds query to API and attempts to retrive data from API provider
+	 * @var string $airportCode 3 char long airport code ex: "LAX"
+	 * @return JSON|false JSON object on success, false on failure
+	 */
+	private function get_weather_forecast($airportCode) 
 	{
-		$jsonString = file_get_contents('http://api.wunderground.com/api/'.$apiKey.'/forecast10day/q/'.$airport.'.json');
+		$jsonString = file_get_contents('http://api.wunderground.com/api/'.$this->apiKey.'/forecast10day/q/'.$airportCode.'.json');
 		$decodedJSON = json_decode($jsonString);
 		
 		// If key JSON object is present and has a value, get weather data from the JSON file
 		if (isset($decodedJSON->{'forecast'}->{'simpleforecast'}->forecastday[0]->{'date'}->{'weekday_short'}) == true) {
 			
-            sleep($apiCallLimitTime);
-            return $decodedJSON;
-            			
-        } else {
-            
-            sleep($apiCallLimitTime+5);
+            sleep($this->apiCallLimitTime);
+            return $decodedJSON;		
+        } else {      
+            sleep($this->apiCallLimitTime+5);
             return false;     
         }
     }
     
-    //extract important data from weather forecast data
-    private function parse_weather_forecast( $data )
+	/**
+	 * Parses JSON file from API provider into format required by database update function
+	 * @param JSON $data JSON|false JSON Object with weather data for airport, false if no data was retrived
+	 */
+    private function parse_weather_forecast($data)
     {
         if ($data == false) {
             return false;
@@ -122,6 +131,19 @@ class WeatherAPI
         }
         
         return $parsedWeather;
+    }
+
+	/**
+	 * Update weather forecast / Store in DB
+	 * @var string $airportCode 3 char long airport code ex: "LAX"
+	 * @var array prepared weather data, see parse_weather_forecaset method
+	 */
+    private function update_database($airportCode, $preparedWeatherData)
+    {
+        print_r($airportCode).PHP_EOL.PHP_EOL;
+        print_r($preparedWeatherData);
+        
+        $this->dataHandler->update_ten_day_forecast($airportCode, $preparedWeatherData);
     }
 
 }
